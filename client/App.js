004c4c3815467c1bd77f9f721733d9bd56e288f4
GustavoNeiraGonzalez <gustavoeduardoneiragonzalez@gmail.com>
2023-04-01 18:33:42 -0300

    agregado obtención de coordenadas fusión de coordenadas y campoos radio y fusion

--- client/App.js
+++ client/App.js
@@ -6,16 +6,22 @@ import * as Location from 'expo-location';
 import haversine from 'haversine';
 
 export default function App() {
+  let timerId;
+
   const example = [
     { "latitude": -33.57151952569935, "longitude": -70.66198445856571 },
     { "latitude": -33.59522924065171, "longitude": -70.67142482846975 },
     { "latitude": -33.604710911424135, "longitude": -70.6145080178976 },
     { "latitude": -33.57073091478793, "longitude": -70.60186006128788 },
     {"latitude": -33.58280176716889, "longitude": -70.6472809240222},
+   
+    {"latitude": -33.580926151345786, "longitude": -70.64625162631273},
+    {"latitude": -33.57931501648034, "longitude": -70.64303398132324}
   ];
   const [location, setLocation] = useState(null);
   const [errorMsg, setErrorMsg] = useState(null);
   const [userLocation, setUserLocation] = useState(null);
+  const [Circles, setCircles] = useState([]);
 
   function handlePress(latitude, longitude)  {
       setUserLocation({latitude,longitude})
@@ -26,76 +32,78 @@ export default function App() {
   
 //--CIRCULOS FUCIONADOS ----
 // Función para calcular la distancia entre dos coordenadas usando la fórmula de Haversine
-function getDistanceInMeters(coord1, coord2) {
-  const R = 6371e3; // radio de la Tierra en metros
-  const lat1 = coord1.latitude * (Math.PI / 180);
-  const lat2 = coord2.latitude * (Math.PI / 180);
-  const deltaLat = (coord2.latitude - coord1.latitude) * (Math.PI / 180);
-  const deltaLng = (coord2.longitude - coord1.longitude) * (Math.PI / 180);
 
-  // Calcular el ángulo central entre las dos coordenadas
-  const a = Math.sin(deltaLat / 2) * Math.sin(deltaLat / 2) +
-            Math.cos(lat1) * Math.cos(lat2) *
-            Math.sin(deltaLng / 2) * Math.sin(deltaLng / 2);
-  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  function getDistanceInMeters(coord1, coord2) {
+    const R = 6371e3; // radio de la Tierra en metros
+    const lat1 = coord1.latitude * (Math.PI / 180);
+    const lat2 = coord2.latitude * (Math.PI / 180);
+    const deltaLat = (coord2.latitude - coord1.latitude) * (Math.PI / 180);
+    const deltaLng = (coord2.longitude - coord1.longitude) * (Math.PI / 180);
 
-  // Calcular la distancia en metros
-  return R * c;
-}
+    // Calcular el ángulo central entre las dos coordenadas
+    const a = Math.sin(deltaLat / 2) * Math.sin(deltaLat / 2) +
+              Math.cos(lat1) * Math.cos(lat2) *
+              Math.sin(deltaLng / 2) * Math.sin(deltaLng / 2);
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
 
-// Función para calcular el centroide de un grupo de coordenadas
-function getCentroid(coords) {
-  let latitudeSum = 0;
-  let longitudeSum = 0;
+    // Calcular la distancia en metros
+    return R * c;
+  }
 
-  // Sumar las latitudes y longitudes de todas las coordenadas del grupo
-  coords.forEach(coord => {
-    latitudeSum += coord.latitude;
-    longitudeSum += coord.longitude;
-  });
+  // Función para calcular el centroide de un grupo de coordenadas
+  function getCentroid(coords) {
+    let latitudeSum = 0;
+    let longitudeSum = 0;
 
-  // Calcular el promedio de las latitudes y longitudes para obtener el centroide
-  return {
-    latitude: latitudeSum / coords.length,
-    longitude: longitudeSum / coords.length
-  };
-}
+    // Sumar las latitudes y longitudes de todas las coordenadas del grupo
+    coords.forEach(coord => {
+      latitudeSum += coord.latitude;
+      longitudeSum += coord.longitude;
+    });
 
-// Agrupar coordenadas cercanas
-const groups = [];
-example.forEach(coord => {
-  let addedToGroup = false;
-  
-  // Verificar si la coordenada está cerca del centroide de algún grupo existente
-  groups.forEach(group => {
-    const centroid = getCentroid(group);
-    if (getDistanceInMeters(coord, centroid) <= 2000) {
-      group.push(coord); // Agregar la coordenada al grupo
-      addedToGroup = true;
-      return;
+    // Calcular el promedio de las latitudes y longitudes para obtener el centroide
+    return {
+      latitude: latitudeSum / coords.length,
+      longitude: longitudeSum / coords.length
+    };
+  }
+
+  // Agrupar coordenadas cercanas
+  const groups = [];
+  example.forEach(coord => {
+    let addedToGroup = false;
+    
+    // Verificar si la coordenada está cerca del centroide de algún grupo existente
+    groups.forEach(group => {
+      const centroid = getCentroid(group);
+      if (getDistanceInMeters(coord, centroid) <= 2000) {
+        group.push(coord); // Agregar la coordenada al grupo
+        addedToGroup = true;
+        return;
+      }
+    });
+    
+    // Si la coordenada no está cerca del centroide de ningún grupo existente,
+    // crear un nuevo grupo con esa coordenada
+    if (!addedToGroup) {
+      groups.push([coord]);
     }
   });
-  
-  // Si la coordenada no está cerca del centroide de ningún grupo existente,
-  // crear un nuevo grupo con esa coordenada
-  if (!addedToGroup) {
-    groups.push([coord]);
-  }
-});
 
-// Calcular el centroide y radio de cada grupo
-const circles = groups.map(group => {
-  const centroid = getCentroid(group); // Calcular el centroide del grupo
-  
-  let radius;
-  if (group.length > 1) {
-    radius = group.length * 80 * .75;
-  } else {
-    radius = 80;
-  }
-  
-  return {...centroid, radius};
-});
+  // Calcular el centroide y radio de cada grupo
+  const circles = groups.map(group => {
+    const centroid = getCentroid(group); // Calcular el centroide del grupo
+    
+    let radius;
+    if (group.length > 1) {
+      radius = group.length * 80 * .75;
+    } else {
+      radius = 80;
+    }
+    
+    return {...centroid, radius};
+  });
+
 
 //--CIRCULOS FUCIONADOS ---------------------------------------------------------------------------
   async function requestLocationPermissions() {
@@ -148,7 +156,63 @@ const circles = groups.map(group => {
   }
   });
 
+  const groupCoordinates = (coordinates, maxDistance, userLocation) => {
+    const groups = [];
+    for (let i = 0; i < coordinates.length; i++) {
+      const start = {
+        latitude: coordinates[i].latitude,
+        longitude: coordinates[i].longitude,
+      };
+      const end = {
+        latitude: userLocation.latitude,
+        longitude: userLocation.longitude,
+      };
+      const distanceInMeters = haversine(start, end, {unit: 'meter'});
+      if (distanceInMeters <= maxDistance) {
+        let grouped = false;
+        for (let j = 0; j < groups.length; j++) {
+          for (let k = 0; k < groups[j].length; k++) {
+            const start = {
+              latitude: coordinates[i].latitude,
+              longitude: coordinates[i].longitude,
+            };
+            const end = {
+              latitude: groups[j][k].latitude,
+              longitude: groups[j][k].longitude,
+            };
+            const distanceInMeters = haversine(start, end, {unit: 'meter'});
+            if (distanceInMeters <= 10) {
+              groups[j].push(coordinates[i]);
+              grouped = true;
+              break;
+            }
+          }
+          if (grouped) break;
+        }
+        if (!grouped) groups.push([coordinates[i]]);
+      }
+    }
+     //aqui se modifica las coordenadas que esten cercas entre si y calcular un punto medio
+    //y reemplazar ambas coordenadas para que solo quede ese punto medio
 
+    return groups.map(group => {
+      const midLatitude = group.reduce((sum, coord) => sum + coord.latitude, 0) / group.length;
+      const midLongitude = group.reduce((sum, coord) => sum + coord.longitude, 0) / group.length;
+      const radio = 90 + (group.length - 1) * 10;
+      const result = {latitude: midLatitude, longitude: midLongitude, radio};
+      if (group.length > 1) result.fusion = group.length;
+      return result;
+    });
+  }
+   
+
+  
+  
+  if (location && location.coords) {
+    const groupedCoordinates = groupCoordinates(example, 2000, location.coords);
+    console.log(groupedCoordinates);
+  }
+    
   return (
     <View style={styles.container}>
       <View style={styles.containermap}>
