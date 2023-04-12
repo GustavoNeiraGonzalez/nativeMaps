8556b3900e7e1e60051db0821913682119215174
GustavoNeiraGonzalez <gustavoeduardoneiragonzalez@gmail.com>
2023-04-12 17:48:24 -0400

    se valida el formato de fecha, se convierte a fecha de tipo django, y en un get se transforma de foormato django a hh:mm dd/mm yyyy

--- backend/Ubicaciones/serializers.py
+++ backend/Ubicaciones/serializers.py
@@ -1,19 +1,23 @@
 from django.contrib.auth import get_user_model
-from rest_framework import serializers, viewsets
-from rest_framework_simplejwt.tokens import RefreshToken
-from django.contrib.auth.tokens import default_token_generator
-from django.core.mail import send_mail
-from django.utils.encoding import force_bytes
-from django.utils.http import urlsafe_base64_encode
-from django.core.validators import validate_email
-from django.core.exceptions import ValidationError
+from rest_framework import serializers
 from .models import Ubicaciones
+from datetime import datetime
 
 User = get_user_model()
 
-
 class UbicacionesSerializer(serializers.ModelSerializer):
+    date = serializers.SerializerMethodField()
+
     class Meta:
         model = Ubicaciones
         fields = ['UbiId', 'crimen', 'date', 'latitude', 'longitude', 'user']
 
+    def validate_date(self, value):
+        try:
+            date = datetime.strptime(value, '%H:%M %d/%m %Y')
+        except ValueError:
+            raise serializers.ValidationError("La fecha no está en el formato correcto (hh:mm dd/mm yyyy)")
+        return date.strftime('%Y-%m-%d %H:%M:%S')
+    def get_date(self, obj):
+        date = datetime.strptime(obj.date, '%Y-%m-%d %H:%M:%S')
+        return date.strftime('%H:%M %d/%m %Y')
