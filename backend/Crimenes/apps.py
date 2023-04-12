a6d268d1c7bdcd4674fce586ce12ab9a8b939af5
GustavoNeiraGonzalez <gustavoeduardoneiragonzalez@gmail.com>
2023-04-12 01:53:50 -0400

    avanze ubicaciones, creado app Crimenes, para que sea un listado de crimenes

---
+++ backend/Crimenes/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class CrimenesConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'Crimenes'
--- backend/Ubicaciones/models.py
+++ backend/Ubicaciones/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
+from .models import Crimenes
 
-# Create your models here.
+class Ubicaciones(models.Model):
+    crimen = models.ForeignKey(Crimenes, on_delete=models.CASCADE)
+    date = models.DateTimeField()
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
