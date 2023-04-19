ff02e06ad108698b48ebf4acd127a24433d3fa8f
GustavoNeiraGonzalez <gustavoeduardoneiragonzalez@gmail.com>
2023-04-19 19:16:33 -0400

    validación de crear un crimen con nombre diferente unicamente

--- backend/Crimenes/serializers.py
+++ backend/Crimenes/serializers.py
@@ -6,3 +6,14 @@ class CrimenesSerializer(serializers.ModelSerializer):
     class Meta:
         model = Crimenes
         fields = ['CrimenId', 'crime']
+
+    def validate(self, data):
+        """
+        Comprueba que no exista un crimen con el mismo nombre.
+        """
+        crime_name = data.get('crime', None)
+        if crime_name:
+            existing_crime = Crimenes.objects.filter(crime=crime_name).first()
+            if existing_crime:
+                raise serializers.ValidationError('Ya existe un crimen con este nombre.')
+        return data
